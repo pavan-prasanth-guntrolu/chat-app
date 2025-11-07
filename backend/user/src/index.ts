e258b281003745093c6fe34fbe2a71a7d8dcffca
Pavan Prasanth <pavanprasanth48850@gmail.com>
2025-11-07 14:53:57 +0530

    redis connection established

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { createClient } from "redis";
 dotenv.config();
 
 connectDB();
+
+if (!process.env.REDIS_URL) {
+  throw new Error("Redis URL not found");
+}
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("connected to Redis"))
+  .catch((error) => console.log(error));
 
 const app = express();
 
