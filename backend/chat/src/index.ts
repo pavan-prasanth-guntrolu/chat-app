e1ba0f148689377d7851b001ed9b5a762fed317f
Pavan Prasanth <pavanprasanth48850@gmail.com>
2025-12-03 19:40:21 +0530

    Restart worked

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -1,15 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import chatRoutes from "./routes/chat.js";
 dotenv.config();
+
 const app = express();
 
-connectDB();
 
-app.use(express.json());
-
-app.use("/api/v1", chatRoutes);
 
 const port = process.env.PORT;
 
