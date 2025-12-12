f00b6491c62b3972b1b0d2d1e8397a8ec3943b1d
Pavan Prasanth <pavanprasanth48850@gmail.com>
2025-12-12 11:29:01 +0530

    faking mail as a server

--- backend/mail/src/index.ts
+++ backend/mail/src/index.ts
@@ -9,6 +9,11 @@ startSendOtpConsumer();
 
 const PORT = process.env.PORT;
 
+// 2. Create a dummy endpoint for health checks
+app.get("/", (req, res) => {
+  res.send("Mail Service is running and listening to RabbitMQ");
+});
+
 app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
 });
